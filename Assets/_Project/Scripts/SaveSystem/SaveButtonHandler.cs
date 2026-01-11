a0386d5f053d1afa93e0951bf546529939f545f6
Titusz <titusz.boros@student.upt.ro>
2026-01-11 19:58:24 +0200

    gamemenu during battles

--- Assets/_Project/Scripts/SaveSystem/SaveButtonHandler.cs
+++ Assets/_Project/Scripts/SaveSystem/SaveButtonHandler.cs
@@ -1,15 +1,37 @@
 using UnityEngine;
-using static UnityEngine.Rendering.DebugUI;
+using UnityEngine.UI;
 
 public class SaveButtonHandler : MonoBehaviour
 {
+    [SerializeField] private Button saveButton;
+
+    private void Awake()
+    {
+        // Auto-assign if not set in inspector
+        if (saveButton == null) saveButton = GetComponent<Button>();
+    }
+
+    private void Update()
+    {
+        if (UIManager.Instance == null) return;
+
+        // The button is interactable ONLY if we are NOT in battle
+        bool canSave = !UIManager.Instance.IsInBattle;
+
+        // Only update if state changed to save performance
+        if (saveButton.interactable != canSave)
+        {
+            saveButton.interactable = canSave;
+        }
+    }
+
     public void OnSaveButtonPressed()
     {
-        // Call the static save system
-        Debug.Log("save button pressed");
+        // Double check just in case
+        if (UIManager.Instance.IsInBattle) return;
 
-        MessageManager.Instance.ShowMessageDirectly($"Game saved");
-
+        Debug.Log("Save button pressed");
+        MessageManager.Instance.ShowMessageDirectly("Game saved");
         SaveSystem.SaveGame();
     }
 }
